ecee39f9e73add3bdbf2e2170b4cd8d53394a41f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-03-01 17:28:32 +0100

    Update 1-redis_op.js

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -2,20 +2,18 @@ import redis from "redis";
 
 const client = redis.createClient();
 client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
-client.on('ready', console.log("Redis client connected to the server"));
+client.on('connect', console.log("Redis client connected to the server"));
 
-client.connect();
 function setNewSchool(schoolName, value){
-  client.set(`${schoolName}`, `${value}`, redis.print);
+  client.set({schoolName, value, (err, reply) => {
+    redis.print(`Reply: ${reply}`);
 }
 
 function displaySchoolValue(schoolName){
-  const value = client.get(`${schoolName}`);
-  console.log = valuue
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
 }
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
-
-client.disconnect();
+displaySchoolValue("Holberton");
+setNewSchool("HolbertonSanFrancisco", "100");
+displaySchoolValue("HolbertonSanFrancisco");
